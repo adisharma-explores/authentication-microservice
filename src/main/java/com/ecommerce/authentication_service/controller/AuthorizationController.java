14dbb5b8ba1d7a7af4ecb95537d03ed348807bb0
adisharma-explores <adisharma191504@gmail.com>
2024-11-30 20:59:20 +0530

    test-check

--- src/main/java/com/ecommerce/authentication_service/controller/AuthorizationController.java
+++ src/main/java/com/ecommerce/authentication_service/controller/AuthorizationController.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v2/auth1")
+@RequestMapping("/api/v2/auth")
 public class AuthorizationController {
     private final CustomerServiceImpl customerService;
     private final JwtUtil jwtUtil;
